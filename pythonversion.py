e0ca30003e59328a836d42698e9ed12e2cc87308
JosephMarch <josephmarch70@gmail.com>
2025-06-25 18:55:17 +0000

    added some user input and the ship class

--- pythonversion.py
+++ pythonversion.py
@@ -1,14 +1,23 @@
 import random
 import time
 import os
-'''
-import keyboard
-'''
-Playing = True
-distance = 0.0
-food = 0
-aliens = 0
+
+distance = 0
+food = 300
+aliens = 30
 speed = 1
+distance = 0
+
+class Ships:
+    def __init__(self,capacity,maxspeed,name):
+        self.capacity = capacity
+        self.maxspeed = maxspeed
+        self.name = name
+
+    def shipstats(self,aliens,speed):
+        print("Ship Name: ", self.name, "\n current capacity: ", aliens,"/",self.capacity, "Current Speed: ", speed,"/",self.maxspeed)
+
+myship = Ships(50,10,'Flying Saucer')
 
 def get_terminal_size():
     try:
@@ -44,8 +53,9 @@ def initscreen(screen, width, height):
     for row in screen:
         print(''.join(row))
 
-def updatescreen(screen, width, height):
+def updatescreen(screen, width, height,food,aliens,speed,distance):
     os.system('clear')
+    print("food: ",food, "\n aliens: ", aliens, "\n speed", speed, "\n distance:", distance)
     for row in screen:
         row.pop(0)
         row.append('*' if random.randint(0,7) == 0 else ' ')
@@ -53,11 +63,35 @@ def updatescreen(screen, width, height):
     for row in screen:
         print(''.join(row))
 
-def playgame(speed):
+def updatefood(food,aliens):
+    for i in range(aliens):
+        food = food - 1
+        if food < 0: 
+            aliens = aliens - 1
+    return food, aliens
+
+def updatedistance(speed,distance):
+    distance = distance + speed
+    return distance 
+
+def buymenu(myship):
+    os.system('clear')
+    print("")
+
+def playgame(speed,food,aliens,distance):
     width, height = get_terminal_size()
     screen = [[' ' for _ in range(width)] for _ in range(height)]
     initscreen(screen, width, height)
-    for i in range(45):
-        time.sleep(0.5)
-        updatescreen(screen,width,height)
-playgame(speed)
+    while aliens > 0:
+        action = input()
+        if action == '':
+            updatescreen(screen,width,height,food,aliens,speed,distance)
+            food , aliens  = updatefood(food,aliens)
+            distance = updatedistance(speed,distance)
+        elif action == 's':
+            myship.shipstats(aliens,speed)
+        else: 
+            continue
+    print("YOU TRAVELLED: ", distance, "light years")
+
+playgame(speed,food,aliens,distance)
