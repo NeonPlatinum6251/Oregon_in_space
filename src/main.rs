5eff3a92e492c858044fe51ba901b15119b16f5f
JosephMarch <josephmarch70@gmail.com>
2025-12-05 12:11:54 +0000

    workinonit
    idk just need github working on linux and this is my test subject

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use term_size;
 use clearscreen;
 use std::{thread, time};
 
+
 fn get_terminal_size() -> (usize, usize) {
     if let Some((w, h)) = term_size::dimensions() {
         (w, h)
@@ -11,7 +12,7 @@ fn get_terminal_size() -> (usize, usize) {
     }
 }
 
-fn draw_ship(screen: &mut Vec<Vec<char>>, width: usize, height: usize) {
+fn draw_ship(screen: &mut Vec<char>, width: usize, height: usize) {
     let ship: [&'static str; 4] = [
         "     ___",
         " ___/   \\___",
@@ -33,7 +34,7 @@ fn draw_ship(screen: &mut Vec<Vec<char>>, width: usize, height: usize) {
                 let row = row_i as usize;
                 let col = col_j as usize;
                 if row < height && col < width {
-                    screen[row][col] = ch;
+                    screen[col+row*width] = ch;
                 }
             }
         }
@@ -49,40 +50,45 @@ fn get_star() -> char {
     }
 }
 
-fn init_screen(screen: &mut Vec<Vec<char>>, width: usize, height: usize) {
+fn init_screen(screen: &mut Vec<char>, width: usize, height: usize) {
     for k in 0..height {
         for l in 0..width {
-            screen[k][l] = get_star()
+            screen[l+k*width] = get_star()
         }
     }
 
     draw_ship(screen, width, height);
     
-    for row in screen.iter() {
-        // collect chars into a String and print
-        let s: String = row.iter().collect();
-        println!("{}", s);
+    for (i, c) in screen.iter().enumerate() {
+        if i%width == 0 {
+            println!("");
+        }
+        print!("{}", c);
     }
 }
 
-fn update_screen(screen: &mut Vec<Vec<char>>,width: usize, height: usize) {
+fn update_screen(screen: &mut Vec<char>,width: usize, height: usize,distance:u64,food:u64,aliens:u64) {
     clearscreen::clear().expect("failed to clear screen");
-    for row in screen.iter_mut() {
-        row.remove(0);
-        row.push(get_star())
+    for i in 0..height {
+        screen.remove(i*width);
+        screen.insert((i+1)*width -1, get_star());
     }
     draw_ship(screen, width, height);
-
-    for row in screen.iter() {
-        // collect chars into a String and print
-        let s: String = row.iter().collect();
-        println!("{}", s);
+    
+    for (i, c) in screen.iter().enumerate() {
+        if i%width == 0 {
+            println!("");
+        }
+        print!("{}", c);
     }
+    make_taskbar(width, distance, food, aliens);
 }
 
 fn make_taskbar(width:usize,distance:u64,food:u64,aliens:u64) {
-    
-}
+    let topandbottom: String = "_".repeat(width-2);
+    println!("{}\n| food: {}\n|distance: {}\n|aliens: {}\n {}",topandbottom,food,distance,aliens,topandbottom)
+    } 
+
 
 fn main() {
     let (w, h) = get_terminal_size();
@@ -98,12 +104,8 @@ fn main() {
         return;
     }
 
-    let mut screen = vec![vec![' '; width]; height];
+    let mut screen = vec![' '; width*height];
 
     init_screen(&mut screen, width, height);
-
-    for gametime in (0..20) {
-        thread::sleep(time::Duration::from_millis(500));
-        update_screen(&mut screen, width, height);
-    }
+    make_taskbar(width, distance, food, aliens);
 }
